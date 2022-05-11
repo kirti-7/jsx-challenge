d4b41d5479c458c270901bf7074a240068a9317a
KirtiValechha <2018.kirti.valechha@ves.ac.in>
2022-05-11 17:07:37 +0530

    friends are life

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<><h1>Shivani Love You</h1>
+  <p>bohat saara</p>
+  <p>SweetHeart 🙈</p>
+  <ol>
+  <li>Aanchal</li>
+  <li>Rohit</li>
+  <li>Kirti</li>
+  <li>Shivani</li>
+  <li>Mayur</li>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </ol>
+</>,
+  document.getElementById("root"));
